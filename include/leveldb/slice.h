ae4bd0bd6fe78bde0779b25cb30622e556d53f4d
yudongdai <41011603@qq.com>
2020-08-18 22:56:44 +0800

    Update slice.h

--- include/leveldb/slice.h
+++ include/leveldb/slice.h
@@ -1,3 +1,4 @@
+6
 // Copyright (c) 2011 The LevelDB Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
@@ -12,12 +13,15 @@
 // non-const method, all threads accessing the same Slice must use
 // external synchronization.
 
+// Slice是一个指向字符串的结构，空间由外部申请，只保存指针和长度
+
 #ifndef STORAGE_LEVELDB_INCLUDE_SLICE_H_
 #define STORAGE_LEVELDB_INCLUDE_SLICE_H_
 
-#include <cassert>
-#include <cstddef>
-#include <cstring>
+#include <assert.h>
+#include <stddef.h>
+#include <string.h>
+
 #include <string>
 
 #include "leveldb/export.h"
@@ -26,31 +30,40 @@ namespace leveldb {
 
 class LEVELDB_EXPORT Slice {
  public:
+  // 默认构造函数，空slice
   // Create an empty slice.
   Slice() : data_(""), size_(0) {}
 
+  // 构造函数
   // Create a slice that refers to d[0,n-1].
   Slice(const char* d, size_t n) : data_(d), size_(n) {}
 
+  // 构造函数
   // Create a slice that refers to the contents of "s"
   Slice(const std::string& s) : data_(s.data()), size_(s.size()) {}
 
+  // 构造函数
   // Create a slice that refers to s[0,strlen(s)-1]
   Slice(const char* s) : data_(s), size_(strlen(s)) {}
 
+  // 复制构造函数和赋值操作符，都是默认的浅拷贝
   // Intentionally copyable.
   Slice(const Slice&) = default;
   Slice& operator=(const Slice&) = default;
 
+  // 返回指向的数据的指针
   // Return a pointer to the beginning of the referenced data
   const char* data() const { return data_; }
 
+  // 返回指向的数据的长度
   // Return the length (in bytes) of the referenced data
   size_t size() const { return size_; }
 
+  // 指向的数据是否为空
   // Return true iff the length of the referenced data is zero
   bool empty() const { return size_ == 0; }
 
+  // 重载下标操作符
   // Return the ith byte in the referenced data.
   // REQUIRES: n < size()
   char operator[](size_t n) const {
@@ -58,12 +71,14 @@ class LEVELDB_EXPORT Slice {
     return data_[n];
   }
 
+  // 清空数据
   // Change this slice to refer to an empty array
   void clear() {
     data_ = "";
     size_ = 0;
   }
 
+  // 截断前n个字符
   // Drop the first "n" bytes from this slice.
   void remove_prefix(size_t n) {
     assert(n <= size());
@@ -71,32 +86,38 @@ class LEVELDB_EXPORT Slice {
     size_ -= n;
   }
 
+  // 构造成一个string对象返回
   // Return a string that contains the copy of the referenced data.
   std::string ToString() const { return std::string(data_, size_); }
 
+  // 比较函数，按字典序
   // Three-way comparison.  Returns value:
   //   <  0 iff "*this" <  "b",
   //   == 0 iff "*this" == "b",
   //   >  0 iff "*this" >  "b"
   int compare(const Slice& b) const;
 
+  // 判断是否以'x'对象为前缀
   // Return true iff "x" is a prefix of "*this"
   bool starts_with(const Slice& x) const {
     return ((size_ >= x.size_) && (memcmp(data_, x.data_, x.size_) == 0));
   }
 
  private:
-  const char* data_;
-  size_t size_;
+  const char* data_; //指针
+  size_t size_; //长度
 };
 
+// 重载==操作符
 inline bool operator==(const Slice& x, const Slice& y) {
   return ((x.size() == y.size()) &&
           (memcmp(x.data(), y.data(), x.size()) == 0));
 }
 
+// 重载!=操作符
 inline bool operator!=(const Slice& x, const Slice& y) { return !(x == y); }
 
+// 比较函数，按字典序
 inline int Slice::compare(const Slice& b) const {
   const size_t min_len = (size_ < b.size_) ? size_ : b.size_;
   int r = memcmp(data_, b.data_, min_len);
